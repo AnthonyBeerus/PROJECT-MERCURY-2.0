cf71cabf77f75ff4c6d1ccb5a7439cb474c3ba95
Anthony Morima jr <95931014+AnthonyBeerus@users.noreply.github.com>
2024-03-17 21:48:51 +0200

    feat: define custom text theme styles
    This commit introduces custom text theme styles to be used throughout the application's user interface. The following text styles have been added:
    
    - displayLarge: A large text style with a font size of 40.0, bold font weight, and a color value of 0xFFF8FAF8 (light gray).
    - displayMedium: A medium text style with a font size of 30.0, bold font weight, and a color value of 0xFFF8FAF8 (light gray).
    - displaySmall: A small text style with a font size of 20.0, bold font weight, and a color value of 0xFFF8FAF8 (light gray).
    - labelSmall: A small label text style with a font size of 15.0, bold font weight, and a color value of 0xFFF8FAF8 (light gray).
    
    These text styles can be used consistently across various parts of the application, such as headings, titles, labels, and other text elements, to maintain a cohesive visual style.
    
    By defining these styles as part of the application's text theme, it becomes easier to manage and update the styles globally if needed, rather than modifying individual text styles throughout the codebase.

--- lib/themes/dark_mode.dart
+++ lib/themes/dark_mode.dart
@@ -8,5 +8,30 @@ ThemeData darkMode = ThemeData(
     secondary: Color(0xFF2d2c2c),
     tertiary: Color(0xFF464645),
     onBackground: Color(0xFFF8FAF8),
+    error: Color(0xffE6BE39),
+    primaryContainer: Color(0xff39E662),
+    inversePrimary: Color(0xff3950E6),
   ),
+  textTheme: const TextTheme(
+    displayLarge: TextStyle(
+      fontSize: 40.0,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFFF8FAF8),
+    ),
+    displayMedium: TextStyle(
+      fontSize: 30.0,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFFF8FAF8),
+    ),
+    displaySmall: TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFFF8FAF8),
+    ),
+    labelSmall: TextStyle(
+      fontSize: 15.0,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFFF8FAF8),
+    ),
+  )
 );
