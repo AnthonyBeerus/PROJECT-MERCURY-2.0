e9d068aebf9ebe3b854e34cf2be4632ac1d57c3d
Anthony Morima jr <95931014+AnthonyBeerus@users.noreply.github.com>
2024-03-17 21:49:49 +0200

    feat: define light mode text theme styles
    This commit introduces custom text theme styles for the light mode of the application's user interface. The following text styles have been added:
    
    - displayLarge: A large text style with a font size of 40.0, bold font weight, and a color value of 0xFF171717 (dark gray/black).
    - displayMedium: A medium text style with a font size of 30.0, bold font weight, and a color value of 0xFF171717 (dark gray/black).
    - displaySmall: A small text style with a font size of 20.0, bold font weight, and a color value of 0xFF171717 (dark gray/black).
    - labelSmall: A small label text style with a font size of 15.0, bold font weight, and a color value of 0xFF171717 (dark gray/black).
    
    These text styles are specifically designed for the light mode of the application, ensuring optimal readability and contrast against a light background.
    
    By defining these styles as part of the application's light mode text theme, it becomes easier to manage and update the styles consistently across various light mode components and screens.
    
    These styles complement the existing dark mode text theme styles, allowing the application to provide a consistent and visually appealing experience for users in both light and dark modes.

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -2,10 +2,35 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
   colorScheme: const ColorScheme.light(
-    background: Color(0xFFF8FAF8),
+    background: Color(0xFFEBEDEB),
     primary: Color(0xFFE63946),
-    secondary: Color(0xFFEBEDEB),
+    secondary: Color(0xFFF8FAF8),
     tertiary: Color(0xFFDFE0DF),
-    onBackground: Color(0xFF171717),
+    onBackground: Color(0xFF151515),
+    error: Color(0xffE6BE39),
+    primaryContainer: Color(0xff39E662),
+    inversePrimary: Color(0xff3950E6)
   ),
+  textTheme: const TextTheme(
+    displayLarge: TextStyle(
+      fontSize: 40.0,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFF171717),
+    ),
+    displayMedium: TextStyle(
+      fontSize: 30.0,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFF171717),
+    ),
+    displaySmall: TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFF171717),
+    ),
+    labelSmall: TextStyle(
+      fontSize: 15.0,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFF171717),
+    ),
+  )
 );
