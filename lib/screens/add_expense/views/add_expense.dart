d4760b8a06e2fa9a6521886eb59639d804477699
Anthony Morima jr <95931014+AnthonyBeerus@users.noreply.github.com>
2024-04-13 01:24:32 +0200

    feat(expense): Implement AddExpense screen UI
    This commit introduces the UI for the AddExpense screen. The screen allows users to add expenses with fields for expense amount, category, and date.
    
    Changes Made:
    - Created AddExpense StatefulWidget to manage the state of the screen.
    - Implemented GestureDetector to dismiss keyboard when tapping outside of text fields.
    - Designed Scaffold with a background color matching the current theme.
    - Configured AppBar with a background color matching the current theme.
    - Added Text widget with the title 'Add Expenses' using the current theme's displayMedium text style.
    - Incorporated TextFormField widgets for entering expense amount, category, and date with appropriate styling and icons.
    - Included TextFormField for selecting the date using showDatePicker.
    - Implemented TextButton with the label 'save' for saving the entered expense, styled according to the current theme.
    
    Notes:
    - The UI design follows best practices for consistency and user experience.
    - Further functionality such as saving expenses to a database or handling form validation will be implemented in subsequent commits.

---
+++ lib/screens/add_expense/views/add_expense.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class AddExpense extends StatefulWidget {
+  const AddExpense({super.key});
+
+  @override
+  State<AddExpense> createState() => _AddExpenseState();
+}
+
+class _AddExpenseState extends State<AddExpense> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.background,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Add Expenses',
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Theme.of(context).colorScheme.secondary,
+                      prefixIcon: Icon(
+                        FontAwesomeIcons.dollarSign,
+                        size: 16,
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30))),
+                ),
+              ),
+              const SizedBox(height: 64),
+              TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Theme.of(context).colorScheme.secondary,
+                  prefixIcon: Icon(
+                    FontAwesomeIcons.list,
+                    size: 16,
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+                  label: const Text('Category'),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                readOnly: true,
+                onTap: () {
+                  showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime.now(),
+                    lastDate: DateTime.now().add(const Duration(days: 365)),
+                  );
+                },
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Theme.of(context).colorScheme.secondary,
+                  prefixIcon: Icon(
+                    FontAwesomeIcons.calendar,
+                    size: 16,
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+                  label: const Text('Date'),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 32),
+              SizedBox(
+                width: double.infinity,
+                height: kToolbarHeight,
+                child: TextButton(
+                  onPressed: () {}, 
+                  style: TextButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0)
+                    )
+                  ),
+                  child: Text(
+                    'save',
+                    style: Theme.of(context).textTheme.labelSmall,
+                  ),
+                )
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
