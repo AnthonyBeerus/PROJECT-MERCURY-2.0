cbac9401b89bb0082035b4d0a4f40a6b7a38069a
Anthony Morima jr <95931014+AnthonyBeerus@users.noreply.github.com>
2024-04-13 01:28:18 +0200

    feat(navigation): Implement navigation to AddExpense screen from HomeScreen
    This commit enhances the HomeScreen by adding functionality to navigate to the AddExpense screen when the FloatingActionButton is pressed. It utilizes the Navigator.push method with a MaterialPageRoute to transition to the AddExpense screen.
    
    Changes Made:
    - Added onPressed callback to the FloatingActionButton.
    - Implemented Navigator.push to transition to the AddExpense screen.
    - Utilized MaterialPageRoute to specify the route and builder for the AddExpense screen.
    
    Notes:
    - This navigation functionality improves user interaction by providing a convenient way to access the AddExpense screen directly from the HomeScreen.

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mercury_2/screens/add_expense/views/add_expense.dart';
 import 'package:mercury_2/screens/home/views/main_screen.dart';
 import 'package:mercury_2/screens/stats/stats.dart';
 
@@ -11,7 +12,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   int index = 0;
   late Color selectedItem = Theme.of(context).colorScheme.primary;
   late Color unselectedItem = Theme.of(context).colorScheme.onBackground;
@@ -29,21 +29,22 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Theme.of(context).colorScheme.background,
         showSelectedLabels: false,
         showUnselectedLabels: false,
-        
         items: [
           BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.money_dollar,
+            icon: Icon(
+              CupertinoIcons.money_dollar,
               color: index == 0
-                ? selectedItem
-                : Theme.of(context).colorScheme.onBackground,
+                  ? selectedItem
+                  : Theme.of(context).colorScheme.onBackground,
             ),
             label: 'Expense',
           ),
           BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.creditcard,
+            icon: Icon(
+              CupertinoIcons.creditcard,
               color: index == 1
-                ? selectedItem
-                : Theme.of(context).colorScheme.onBackground,
+                  ? selectedItem
+                  : Theme.of(context).colorScheme.onBackground,
             ),
             label: 'Transactions',
           ),
@@ -51,7 +52,14 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         elevation: 0,
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(
+            context, 
+            MaterialPageRoute<void>(
+              builder: (BuildContext context) => const AddExpense()
+            ),
+          );
+        },
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10),
         ),
@@ -59,9 +67,7 @@ class _HomeScreenState extends State<HomeScreen> {
         child: const Icon(CupertinoIcons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      body: index == 0
-          ? const MainScreen()
-          : const StatsScreen(),
+      body: index == 0 ? const MainScreen() : const StatsScreen(),
     );
   }
 }
